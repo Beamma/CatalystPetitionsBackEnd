2ba9bf6c071a6e8c75d0f1289f8c29ded10bb86b
Joel Bremner <jbr257@uclive.ac.nz>
2024-03-21 03:53:24 +1300

    POST to support now working in full

--- src/app/controllers/petition.supporter.controller.ts
+++ src/app/controllers/petition.supporter.controller.ts
@@ -1,7 +1,12 @@
 import {Request, Response} from "express";
 import Logger from "../../config/logger";
+import * as users from '../models/user.model';
 import * as petitions from '../models/petition.model';
 import * as supporters from '../models/supporter.model';
+import * as supportTiers from '../models/supportTiers.model';
+import { validateSession } from "./user.controller";
+import {validate} from '../../config/ajv';
+import * as schemas from '../resources/schemas.json';
 
 
 const getAllSupportersForPetition = async (req: Request, res: Response): Promise<void> => {
@@ -27,9 +32,49 @@ const getAllSupportersForPetition = async (req: Request, res: Response): Promise
 
 const addSupporter = async (req: Request, res: Response): Promise<void> => {
     try{
-        // Your code goes here
-        res.statusMessage = "Not Implemented Yet!";
-        res.status(501).send();
+        const id = req.params.id;
+        const petition = await petitions.getById(id);
+        const tierId = req.body.supportTierId;
+        const tier = await supportTiers.getById(tierId);
+        if (petition.length === 0) {
+            res.status(404).send(`petition does not exist`);
+            return;
+        }
+
+        const webToken = req.headers['x-authorization'];
+        if (webToken === undefined) {
+            res.status(401).send('Unauthorized');
+            return;
+        }
+        if (await validateSession(petition[0].owner_id.toString(), req)) {
+            res.status(403).send('Cannot support your own petition');
+            return;
+        }
+
+        const validation = await validate(
+            schemas.supporters_post, req.body
+        );
+        if (validation !== true) {
+            res.statusMessage = `Bad Request: ${validation.toString()}`; // ChecK?
+            res.status(400).send('Failed');
+            return;
+        }
+
+        if (tier.length === 0) {
+            res.status(404).send(`Suppoerter Tier does not exist`);
+            return;
+        }
+
+        const user = await users.getByToken(webToken.toString());
+
+        if (await checkForAlreadySupport(user[0].id, tierId)) {
+            res.status(403).send('You already support this tier');
+            return;
+        }
+
+        const result = await supporters.insert(req, id, tierId, user[0].id)
+
+        res.status(201).send();
         return;
     } catch (err) {
         Logger.error(err);
@@ -37,6 +82,16 @@ const addSupporter = async (req: Request, res: Response): Promise<void> => {
         res.status(500).send();
         return;
     }
+}
+
+const checkForAlreadySupport = async (userId: string, tierId: string): Promise<boolean> => {
+
+    const result = await supporters.checkSupport(userId, tierId);
+    if (result.length === 0) {
+        return false;
+    }
+
+    return true;
 }
 
 export {getAllSupportersForPetition, addSupporter}
--- src/app/models/supporter.model.ts
+++ src/app/models/supporter.model.ts
@@ -34,5 +34,24 @@ const getByTierId = async (id: string) => {
     return result;
 }
 
+const checkSupport = async (userId: string, tierId: string) => {
+    Logger.info(`Checking for user ${userId} and tier ${tierId}`);
+    const conn = await getPool().getConnection();
+    const query = 'SELECT * FROM supporter WHERE support_tier_id = (?) AND user_id = (?)';
+    const [result] = await conn.query(query,[tierId, userId]);
+    await conn.release();
+    return result;
+}
 
-export {getByPetitionId, getByTierId, getByPetitionIdReversed}
+const insert = async (req: Request, petitionId: string, tierId: string, userId: string): Promise<ResultSetHeader> => {
+    Logger.info(`Adding supporter to the database`);
+    const conn = await getPool().getConnection();
+    const now = new Date();
+    const sqlDatetime = now.toISOString().slice(0, 19).replace('T', ' ');
+    const query = 'insert into supporter (petition_id, support_tier_id, user_id, message, timestamp) values (?, ?, ?, ?, ?)';
+    const [result] = await conn.query(query,[petitionId, tierId, userId, req.body.message, sqlDatetime]);
+    await conn.release();
+    return result;
+}
+
+export {getByPetitionId, getByTierId, getByPetitionIdReversed, checkSupport, insert}
